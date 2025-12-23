48fd3fe21a7e6d153cb0318a4fc1e7d57ba64c4e
Spandana Munagala <s.spandanamunagala@gmail.com>
2025-12-23 21:55:42 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,32 @@
+<?php
+session_start();
+include "config.php";
 
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare(
+        "SELECT * FROM users WHERE email=?"
+    );
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['role'] = $user['role'];
+        header("Location: dashboard.php");
+    } else {
+        echo "Invalid login";
+    }
+}
+?>
+
+<form method="post">
+  <input name="email" type="email" required><br>
+  <input name="password" type="password" required><br>
+  <button name="login">Login</button>
+</form>
