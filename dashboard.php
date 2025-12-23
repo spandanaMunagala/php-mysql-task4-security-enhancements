e3b282be9572196357ae6da284d1fb7f9646a24a
Spandana Munagala <s.spandanamunagala@gmail.com>
2025-12-23 21:55:18 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,16 @@
+<?php
+session_start();
 
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
+
+echo "Welcome User<br>";
+echo "Role: " . $_SESSION['role'];
+
+if ($_SESSION['role'] === 'admin') {
+    echo "<br><a href='admin.php'>Admin Panel</a>";
+}
+echo "<br><a href='logout.php'>Logout</a>";
+?>
